1f034e369e5e8356a53e410f109b04f81d7b41f7
Joffrey Lagut <j.lagut@live.fr>
2023-10-07 22:40:59 +0200

    feat: initial commit with home and projects
    Displays a home page and a projects page available through a menu.
    
    Some work in progress are also included:
    - English version
    - Blog with articles
    - Project: MLOPS Product Classification

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+export default {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
---
+++ src/i18n/ui.ts
@@ -0,0 +1,143 @@
+export const languages = {
+  en: "Show website in English",
+  fr: "Afficher le site en Français",
+};
+
+export const languagesIcons = {
+  en: "united_kingdom",
+  fr: "france"
+}
+
+export const defaultLang = "fr";
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.work": "Work",
+    "nav.about": "About",
+    "work.baselayout.title": "My projects | Joffrey Lagut",
+    "work.baselayout.description": "Learn about Joffrey Lagut's most recent projects",
+    "work.hero.title": "My projects",
+    "work.hero.tagline": "See my most recent projects below.",
+    "blog.baselayout.title": "My blog | Joffrey Lagut",
+    "blog.baselayout.description": "Learn about Joffrey Lagut's most recent posts",
+    "blog.hero.title": "My blog",
+    "blog.hero.tagline": "See my most recent posts below.",
+
+    "home.hero.title": "Hello there, my name is Joffrey Lagut 👋🏻",
+    "home.hero.tagline": "I am an IT engineer who is currently based in Dijon, France.",
+    "home.hero.roles.first": "Developer",
+    "home.hero.roles.second": "MLOPS",
+    "home.hero.roles.third": "Project manager",
+    "home.hero.img_alt": "Joffrey's profile picture",
+
+    "home.work.title": "Selected projects",
+    "home.work.subtitle": "Take a look below at some of my featured projects.",
+    
+    "home.articles.title": "Articles",
+    "home.articles.subtitle": "I like to share some of my thoughts with my pairs. Sometimes only remarks, other times solutions to issues I faced.",
+
+    "contact.title": "Interested in working together?",
+    "contact.send_message": "Send me a message",
+
+        "nav.home": "Accueil",
+    "nav.work": "Projets",
+    "nav.blog": "Blog",
+    "nav.about": "À propos",
+    "nav.twitter": "Twitter",
+
+    "work.baselayout.title": "Mes projets | Joffrey Lagut",
+    "work.baselayout.description": "En savoir plus sur les projets les plus récents",
+    "work.hero.title": "Mes projets",
+    "work.hero.tagline": "Ci-dessous mes projets les plus récents.",
+
+    "blog.baselayout.title": "Mon blog | Joffrey Lagut",
+    "blog.baselayout.description": "En savoir plus sur les articles les plus récents",
+    "blog.hero.title": "Mon blog",
+    "blog.hero.tagline": "Ci-dessous mes derniers articles publiés.",
+
+    "home.hero.title": "Hello, je m'appelle Joffrey Lagut 👋🏻",
+    "home.hero.tagline": "Je suis un passionné de technologie vivant actuellement à Dijon.",
+    "home.hero.roles.first": "Ingénieur",
+    "home.hero.roles.second": "Liseur",
+    "home.hero.roles.third": "Cinéphile",
+    "home.hero.img_alt": "Photo de profil de Joffrey",
+
+    "home.skills.top1.title": "Ingénieur IT",
+    "home.skills.top1.caption": "Analyse du besoin, prototypage, développement et mise en production d'applications métiers.",
+
+    "home.skills.top2.title": "Data Science",
+    "home.skills.top2.caption": "Analyse et visualisation des données. Développement, mise en production et monitoring de modèles d'intelligence artificielle.",
+
+    "home.skills.top3.title": "Robotique",
+    "home.skills.top3.caption": "Analyse des processus et identification des parties automatisables. Développement et mise en production des robots.",
+
+    "home.button.view_all": "Consulter la liste",
+
+    "home.work.title": "Réalisations récentes",
+    "home.work.subtitle": "Jetez un œil sur quelques un de mes projets récents.",
+    
+    "home.blog.title": "Articles",
+    "home.blog.subtitle": "J'aime partager mes pensées avec mes pairs. Parfois des remarques, d'autres fois des solutions à certains problèmes que j'ai rencontré.",
+
+    "home.skill.title": "Compétences clés",
+    "home.skill.subtitle": "Ma passion pour la technologie me pousse toujours à apprendre de nouvelles choses.Voici une sélection de compétences que j'utilise au quotidien :",
+
+    "contact.title": "Vous souhaitez me contacter ?",
+    "contact.send_message": "Envoyez moi un message",
+
+    "footer.quote": "“Luck is what happens when preparation meets opportunity.” –Seneca"
+
+  },
+  fr: {
+    "nav.home": "Accueil",
+    "nav.work": "Projets",
+    "nav.blog": "Blog",
+    "nav.about": "À propos",
+    "nav.twitter": "Twitter",
+
+    "work.baselayout.title": "Mes projets | Joffrey Lagut",
+    "work.baselayout.description": "En savoir plus sur les projets les plus récents",
+    "work.hero.title": "Mes projets",
+    "work.hero.tagline": "Ci-dessous mes projets les plus récents.",
+
+    "blog.baselayout.title": "Mon blog | Joffrey Lagut",
+    "blog.baselayout.description": "En savoir plus sur les articles les plus récents",
+    "blog.hero.title": "Mon blog",
+    "blog.hero.tagline": "Ci-dessous mes derniers articles publiés.",
+
+    "home.hero.title": "Hello, je m'appelle Joffrey Lagut 👋🏻",
+    "home.hero.tagline": "Je suis un passionné de technologie vivant actuellement à Dijon.",
+    "home.hero.roles.first": "Ingénieur",
+    "home.hero.roles.second": "Liseur",
+    "home.hero.roles.third": "Cinéphile",
+    "home.hero.img_alt": "Photo de profil de Joffrey",
+
+    "home.skills.top1.title": "Ingénieur IT",
+    "home.skills.top1.caption": "Analyse du besoin, prototypage, développement et mise en production d'applications métiers.",
+
+    "home.skills.top2.title": "Data Science",
+    "home.skills.top2.caption": "Analyse et visualisation des données. Développement, mise en production et monitoring de modèles d'intelligence artificielle.",
+
+    "home.skills.top3.title": "Robotique",
+    "home.skills.top3.caption": "Analyse des processus et identification des parties automatisables. Développement et mise en production des robots.",
+
+    "home.button.view_all": "Consulter la liste",
+
+    "home.work.title": "Réalisations récentes",
+    "home.work.subtitle": "Jetez un œil sur quelques un de mes projets récents.",
+    
+    "home.blog.title": "Articles",
+    "home.blog.subtitle": "J'aime partager mes pensées avec mes pairs. Parfois des remarques, d'autres fois des solutions à certains problèmes que j'ai rencontré.",
+
+    "home.skill.title": "Compétences clés",
+    "home.skill.subtitle": "Ma passion pour la technologie me pousse toujours à apprendre de nouvelles choses.Voici une sélection de compétences que j'utilise au quotidien :",
+
+    "contact.title": "Vous souhaitez me contacter ?",
+    "contact.send_message": "Envoyez moi un message",
+
+    "footer.quote": "“La chance est ce qui arrive quand la préparation coïncide avec l'opportunité.” –Sénèque"
+  },
+} as const;
+
