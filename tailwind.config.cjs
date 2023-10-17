3bf370f5ab34002671fb72f928285680d7e4b54a
Joffrey Lagut <j.lagut@live.fr>
2023-10-17 22:18:16 +0200

    refactor: add all accent colors to tailwindcss theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors:{
+                accent:{
+                    light: "rgb(var(--accent-light)/<alpha-value>)",
+                    regular: "rgb(var(--accent-regular)/<alpha-value>)",
+                    dark: "rgb(var(--accent-dark)/<alpha-value>)",
+                    overlay: "rgb(var(--accent-overlay)/<alpha-value>)",
+                    subtleOverlay: "rgb(var(--accent-subtle-overlay)/<alpha-value>)",
+                    textOver: "rgb(var(--accent-text-over)/<alpha-value>)",
+
+                },
+            },
+        },
+    },
+    plugins: [],
 }
